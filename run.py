8734b3bd7ec33acbe535bd7fe3872df8698b4b68
Marcin Olszewski <cinula@gmail.com>
2024-05-29 13:01:23 +0000

    creating instruction for battleship game

--- run.py
+++ run.py
@@ -1,64 +1,95 @@
 import random
 
-# Main Variable 
 
-GAME_BOARD_SIZE = 5
+# Constants
+
+BOARD_SIZE = 5
 SHIPS_COUNTER = 3
 SHOTS = 10
 
-# Creating Game board 
+# Create the board
 
-board = [["-"] * GAME_BOARD_SIZE for _ in range(GAME_BOARD_SIZE)]
+board = [['-'] * BOARD_SIZE for _ in range(BOARD_SIZE)]
 
 def print_board(board):
 
-    """ Creating game board """
+    """creating game board"""
 
     for row in board:
         print(' '.join(row))
 
 def place_ships(board, num_ships):
 
-    """ Placeing ships randomly on game board """
-
+    """ Placeing ships on the board """
+    
     ships = []
     while len(ships) < num_ships:
-        row = random.randint(0, GAME_BOARD_SIZE - 1)
-        col = random.randint(0, GAME_BOARD_SIZE - 1)
+        row = random.randint(0, BOARD_SIZE - 1)
+        col = random.randint(0, BOARD_SIZE - 1)
         if (row, col) not in ships:
-            ships.append((row,col))
-    return(ships)
+            ships.append((row, col))
+    return ships
 
 def get_player_guess():
     
-    """ Function for player to shoot """
-
+    """ Function for player to choose where to shoot"""
+    
     try:
-        row = int(input("Gues Row (0-4): "))
-        col = int(input("Gues column (0-4): "))
+        row = int(input("Guess Row (0-4): "))
+        col = int(input("Guess Column (0-4): "))
         return row, col
     except ValueError:
-        print("Please enter valid number.")
-        return get_player_guess() 
+        print("Please enter valid integers.")
+        return get_player_guess()
 
 def check_guess(guess, ships):
 
-    """ Checking if player hit battleship or miss """ 
+    """ Game checks if was hit or miss"""
 
     if guess in ships:
         ships.remove(guess)
-        return "HIT!"
+        return "Hit!"
     else:
         return "Miss!"
 
-def update_board():
+def update_board(board, guess, result):
 
-    """ Function for update the gameboard """
+    """ Updeting game board with miss or hit"""
+
+    row, col = guess
+    if result == "Hit!":
+        board[row][col] = "X"
+    else:
+        board[row][col] = "O"
 
 def main():
 
-    """Main game engine fuction for battlship game to run"""
+    """ This is main game engine """
 
-
-
-
+    ships = place_ships(board, SHIPS_COUNTER)
+    print("Welcome to Battleship!")
+    print("x" * 25)
+    print("1.You have 10 Turns (shots)\nto destroy all\nenemies battleships!")
+    print("2.Below the game board you\nsee guess row option\npick the row number from 0 to 4\nautomaticly shows you another option\nto choose column\nPlease pick columnt number\nfrom 0 - 4 ")
+    print("3.Game board shows 'O' for miss and 'X' for hit.\n4.Under game board you can see\nGame counter\nHow meny turens you made\nBest of Luck!" )
+    print("x" * 25)
+    print_board(board)
+    turns = 0
+    while turns < SHOTS and ships:
+        guess = get_player_guess()
+        if 0 <= guess[0] < BOARD_SIZE and 0 <= guess[1] < BOARD_SIZE:
+            result = check_guess(guess, ships)
+            update_board(board, guess, result)
+            print(result)
+        else:
+            print("Out of bounds. Try again.")
+        print_board(board)
+        turns += 1
+        print(f"Your heve: {SHOTS} turns\nYour turns counter: {turns}" )
+    if not ships:
+        print("Congratulations! You sunk all the ships!")
+    else:
+        print("Game Over. You ran out of turns.")
+        print("Ships were at:", ships)
+if __name__ == "__main__":
+    main()
